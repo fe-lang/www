5c429edb4a9f9f04e1fdd43656039b421306b367
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2025-12-12 11:37:53 +0000

    Github Actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://fe-lang.github.io',
+	base: '/www/',
 	redirects: {
 		'/': '/getting-started/what-is-fe/',
 	},
